7eeb2d919dadfc0faf1afd509c465e6de04e198b
Christiano831 <cespinola@live.com>
2022-07-04 15:38:31 -0700

    add variable types

--- Unity Start/Assets/Scripts/LearningCurve.cs
+++ Unity Start/Assets/Scripts/LearningCurve.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    public int currentAge = 30;
+    // Integer variables
+    private int currentAge = 30;
+    public int addedAge = 1;
+    
+    //float ends with f
+    public float pi = 3.14f;
+
+    //string
+    public string firstName = "Bob";
+
+    //boolean
+    public bool isAuthor = true;
 
     // Start is called before the first frame update
     void Start()
@@ -12,11 +23,17 @@ public class LearningCurve : MonoBehaviour
         Debug.Log(30 + 1);
 
         Debug.Log(currentAge + 1);
+        ComputedAge();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Function
+    void ComputedAge(){
+        Debug.Log(currentAge + addedAge);
     }
 }
